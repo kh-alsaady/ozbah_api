6054e47a0044140cbf80e6e6b586639c4c11d0a3
Khalid <kh.alsaady@gmail.om>
2016-07-24 14:14:23 +0530

    code added

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::SessionsController < Api::V1::BaseController
   skip_before_action :authenticate, only: [:create]
 
   def create
-    
+
     missing_params = check_required_params params, [:login, :password]
     return  render_response false, I18n.t('missing_params') + missing_params.join(', '), {}, 400 unless missing_params.empty?
 
@@ -17,12 +17,13 @@ class Api::V1::SessionsController < Api::V1::BaseController
   end
 
   def destroy
-    access_token = request.headers[:HTTP_ACCESS_TOKEN]
-    if User.destroy_session access_token
-      render_response true, I18n.t('logout_success'), {}, 200
-    else
-      render_response false, I18n.t('logout_success'), {}, 200
-    end
+    return render_response true, 'success', {}, 200
+    # access_token = request.headers[:HTTP_ACCESS_TOKEN]
+    # if User.destroy_session access_token
+    #   render_response true, I18n.t('logout_success'), {}, 200
+    # else
+    #   render_response false, I18n.t('logout_success'), {}, 200
+    # end
 
   end
 end
