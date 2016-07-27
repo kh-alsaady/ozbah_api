4d9ac98b8d20b238c246a11d415b7f13a1d93829
Khalid Alsaady <k.alsaady@clickapps.co>
2016-07-27 18:52:53 +0530

    email config added

--- config/environments/dev.rb
+++ config/environments/dev.rb
@@ -83,4 +83,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Send mail configurations
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'localhost:3000'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: '587',
+    domain: 'gmail.com',
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: "ozbah.india@gmail.com",
+    password: 'ozbah7340888144'
+  }
 end
