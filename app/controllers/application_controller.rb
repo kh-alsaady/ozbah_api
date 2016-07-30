f98a618986f3a0e859950e7a58d05afd28645964
Khalid <kh.alsaady@gmail.om>
2016-07-30 23:05:49 +0530

    add options http verb code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+
+  # Action to follow routes with HTTP verb 'OPTIONS'
+  def cors
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token, Content-Type'
+    headers['Access-Control-Max-Age'] = '1728000'
+
+    render text: '', content_type: 'text/plain'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,6 @@ Rails.application.routes.draw do
     end
   end
 
+  match '*all' => 'application#cors', via: :options
+
 end
