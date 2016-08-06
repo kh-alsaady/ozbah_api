9b043eafaeb91056f6f3949bfd85e2273c57145d
Khalid <kh.alsaady@gmail.om>
2016-08-06 18:42:45 +0530

    code added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::API
   def cors
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token, Content-Type'
+    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token, Content-Type, access_token'
     headers['Access-Control-Max-Age'] = '1728000'
 
     render text: '', content_type: 'text/plain'
@@ -13,18 +13,18 @@ class ApplicationController < ActionController::API
 
 
   # This method will set the access control header accordingly after each action performed.
-	# def cors_set_access_control_headers
-  # 	headers['Access-Control-Allow-Origin'] = '*'
-  # 	headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-  # 	headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
-  # 	headers['Access-Control-Max-Age'] = "1728000"
-  # end
+	def cors_set_access_control_headers
+  	headers['Access-Control-Allow-Origin'] = '*'
+  	headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+  	headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+  	headers['Access-Control-Max-Age'] = "1728000"
+  end
 
-def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, Authorization, X-Prototype-Version, Token, Content-Type'
-    headers['Access-Control-Max-Age'] = "1728000"
-end
+# def cors_set_access_control_headers
+#     headers['Access-Control-Allow-Origin'] = '*'
+#     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+#     headers['Access-Control-Allow-Headers'] = 'X-Requested-With, Authorization, X-Prototype-Version, Token, Content-Type'
+#     headers['Access-Control-Max-Age'] = "1728000"
+# end
 
 end
