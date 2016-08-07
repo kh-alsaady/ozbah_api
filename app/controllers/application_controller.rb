5a10620f343c40013da12ab8cb46cf48d97aa8a6
Khalid <kh.alsaady@gmail.om>
2016-08-07 21:52:56 +0530

    fix cors issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,21 +11,23 @@ class ApplicationController < ActionController::API
     render text: '', content_type: 'text/plain'
   end
 
-
   # This method will set the access control header accordingly after each action performed.
-	def cors_set_access_control_headers
-  	headers['Access-Control-Allow-Origin'] = '*'
-  	headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-  	headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
-    headers['Access-Control-Allow-Headers'] = '*'
-  	headers['Access-Control-Max-Age'] = "1728000"
-  end
+	# def cors_set_access_control_headers
+  # 	headers['Access-Control-Allow-Origin'] = '*'
+  # 	headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+  # 	headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+  #   headers['Access-Control-Allow-Headers'] = '*'
+  # 	headers['Access-Control-Max-Age'] = "1728000"
+  # end
 
-# def cors_set_access_control_headers
-#     headers['Access-Control-Allow-Origin'] = '*'
-#     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-#     headers['Access-Control-Allow-Headers'] = 'X-Requested-With, Authorization, X-Prototype-Version, Token, Content-Type'
-#     headers['Access-Control-Max-Age'] = "1728000"
-# end
+def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, Authorization, X-Prototype-Version, Token, Content-Type'
+    headers['Access-Control-Max-Age'] = "1728000"
+end
+
+# Fetch API cannot load http://52.32.142.241/api/v1/profile/. No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'http://localhost:8080' is therefore not allowed access. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled
+
 
 end
