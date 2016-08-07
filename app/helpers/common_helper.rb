c8ea42a4d8bc9a5beb5fb1faa980e91a3b4fd623
Khalid <kh.alsaady@gmail.om>
2016-08-07 17:19:49 +0530

    enable send email

--- app/helpers/common_helper.rb
+++ app/helpers/common_helper.rb
@@ -6,10 +6,10 @@ module CommonHelper
       next_user = ArchivedTask.next_user task.id
 
       task_type = task.task_type.name.downcase
-      #  NotifierMailer.notify_user_for_task(task, next_user).deliver_now
+       NotifierMailer.notify_user_for_task(task, next_user).deliver_now
 
-      print(task.task_type.name, ': ', task.name,'===========>', next_user.full_name )
-      puts
+      # print(task.task_type.name, ': ', task.name,'===========>', next_user.full_name )
+      # puts
 
       current_archive = ArchivedTask.find_by(user_id: next_user.id, task_id: task.id, status: 0)
       ArchivedTask.create(user_id: next_user.id, task_id: task.id, status: 1) unless current_archive
